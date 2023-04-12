b263f3f824469346ff1bc45a34577bd4e72ed9f3
paulozemr <pauloramosj213@gmail.com>
2023-04-12 11:44:31 -0300

    Coisas

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Comentem seus buubo</Text>
       {/* Comentem , as coisas que voces fazem , e expliquem os commits  */}
+      {/* Estou adicionando coisas por Yarn no windows , é bem mais facil de fazer assim pq é mais pratico de digitar  */}
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
