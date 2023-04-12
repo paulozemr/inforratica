046434302978a01dec1d3ed1bf774b86ef89e235
paulozemr <pauloramosj213@gmail.com>
2023-04-12 11:28:46 -0300

    First commit , projeto inicializado

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Comentem seus buubo</Text>
+      {/* Comentem , as coisas que voces fazem , e expliquem os commits  */}
       <StatusBar style="auto" />
     </View>
   );
